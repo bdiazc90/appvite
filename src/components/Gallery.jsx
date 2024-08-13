51e00559c27add936cff7628f6c79ce728c0c03a
bdiazc90 <bdiazc90@gmail.com>
2024-08-12 22:19:28 -0500

    fix: semantic variable names + add .editorconfig

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,32 +1,42 @@
-import beast from "../consts/hornedBeast.json"
+import hornedBeasts from "../consts/hornedBeast.json";
 
 const HornedBeast = (props) => {
     return (
         <>
-            <article >
+            <article>
                 <h2>{props.title}</h2>
                 <p>{props.description}</p>
-                <img key={props.id} alt={props.title} src={props.image_url} />
+                <img alt={props.title} src={props.image_url} />
                 <p>{props.description}</p>
             </article>
         </>
     );
-}
+};
 
 const Gallery = () => {
     return (
         <>
-            <HornedBeast title="Alex" description="tengo 28" image_url="https://placehold.co/200x200" />
-            <HornedBeast title="Bruno" description="tengo 33" image_url="https://placehold.co/200x200" />
-        
-            {beast.map(data => (
-                <div key={data._id}>
-                    <HornedBeast title={data.title} image_url={data.image_url} description={data.description} />
-                </div>
-            ))
-            }
+            <HornedBeast
+                title="Alex"
+                description="tengo 28"
+                image_url="https://placehold.co/200x200"
+            />
+            <HornedBeast
+                title="Bruno"
+                description="tengo 33"
+                image_url="https://placehold.co/200x200"
+            />
+
+            {hornedBeasts.map((hornedBeast) => (
+                <HornedBeast
+                    key={hornedBeast._id}
+                    title={hornedBeast.title}
+                    image_url={hornedBeast.image_url}
+                    description={hornedBeast.description}
+                />
+            ))}
         </>
     );
-}
+};
 
 export default Gallery;
