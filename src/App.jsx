23744ca177468088e61772d6b3224c3d43aa89e1
AGV <alexander@enter.edu.pe>
2024-08-12 21:42:33 -0500

    feature:map

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./components/Header"
 import Gallery from "./components/Gallery"
 import Footer from "./components/Footer"
 
+
 function App() {
   const [count, setCount] = useState(0)
 
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,22 +1,32 @@
-
+import beast from "../consts/hornedBeast.json"
 
 const HornedBeast = (props) => {
     return (
-        <article>
-            <h2>{props.title}</h2>
-            <p>{props.description}</p>
-            <img src={props.imageUrl} alt={props.title} />
-        </article>
-    )
+        <>
+            <article >
+                <h2>{props.title}</h2>
+                <p>{props.description}</p>
+                <img key={props.id} alt={props.title} src={props.image_url} />
+                <p>{props.description}</p>
+            </article>
+        </>
+    );
 }
 
 const Gallery = () => {
     return (
         <>
-            <HornedBeast title="Alex" description="tengo 28" imageUrl="https://placehold.co/200x200" />
-            <HornedBeast title="Bruno" description="tengo 33" imageUrl="https://placehold.co/200x200" />
+            <HornedBeast title="Alex" description="tengo 28" image_url="https://placehold.co/200x200" />
+            <HornedBeast title="Bruno" description="tengo 33" image_url="https://placehold.co/200x200" />
+        
+            {beast.map(data => (
+                <div key={data._id}>
+                    <HornedBeast title={data.title} image_url={data.image_url} description={data.description} />
+                </div>
+            ))
+            }
         </>
-    )
+    );
 }
 
 export default Gallery;
